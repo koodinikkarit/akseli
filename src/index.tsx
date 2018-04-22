4cc445719e57cba24825e20e218cb5aac3faf00f
jaska <pusanteemu@gmail.com>
2018-04-22 16:15:32 +0300

    Päivitetty petriClientin luontia.

--- src/index.tsx
+++ src/index.tsx
@@ -12,22 +12,34 @@ import { ApolloProvider } from "react-apollo";
 import registerServiceWorker from "./registerServiceWorker";
 
 import { createPetriClient } from "petri-client";
+import { resolvers, defaults } from "./resolvers";
 
-const developmentEnvironment = process.env.NODE_ENV === "development";
+const developmentEnvironment =
+	process && process.env && process.env.NODE_ENV === "development";
+
+const graphqlHost = (developmentEnvironment
+	? process.env.REACT_APP_GRAPHQL_HOST
+	: window.location.host) as string;
+
+const graphqlPort = (developmentEnvironment
+	? parseInt(process.env.REACT_APP_GRAPHQL_PORT as string, 10)
+	: parseInt(window.location.port, 10)) as number;
+
+const graphqlSubscriptionsHost = (developmentEnvironment
+	? process.env.REACT_APP_GRAPHQL_HOST
+	: window.location.host) as string;
+
+const graphqlSubscriptionsPort = developmentEnvironment
+	? parseInt(process.env.REACT_APP_GRAPHQL_PORT as string, 10)
+	: parseInt(window.location.port, 10);
 
 const client = createPetriClient({
-	graphqlHost: developmentEnvironment
-		? (process.env.REACT_APP_GRAPHQL_HOST as string)
-		: window.location.host,
-	graphqlPort: developmentEnvironment
-		? parseInt(process.env.REACT_APP_GRAPHQL_PORT as string, 10)
-		: parseInt(window.location.port, 10),
-	graphqlSubscriptionsHost: developmentEnvironment
-		? (process.env.REACT_APP_GRAPHQL_HOST as string)
-		: window.location.host,
-	graphqlSubscriptionsPort: developmentEnvironment
-		? parseInt(process.env.REACT_APP_GRAPHQL_PORT as string, 10)
-		: parseInt(window.location.port, 10)
+	graphqlHost,
+	graphqlPort,
+	graphqlSubscriptionsHost,
+	graphqlSubscriptionsPort,
+	resolvers: resolvers,
+	defaults: defaults
 });
 
 ReactDOM.render(
---
+++ src/resolvers/index.ts
@@ -0,0 +1,6 @@
+import merge from "lodash.merge";
+
+const merged = merge({});
+
+export const resolvers = merged.resolvers;
+export const defaults = merged.defaults;
